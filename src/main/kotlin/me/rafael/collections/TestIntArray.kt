08d55f46a6909aafe29eb96828cd4710b2c2e31a
rafael <rafaeldutraribas@gmail.com>
2021-09-07 14:44:01 -0300

    funcionamento do IntArray e maneiras de varrer

---
+++ src/main/kotlin/me/rafael/collections/TestIntArray.kt
@@ -0,0 +1,26 @@
+package me.rafael.collections
+
+fun main() {
+    val value = IntArray(5)
+
+    // iniciar valores
+    value[0] = 1
+    value[1] = 4
+    value[2] = 3
+    value[3] = 5
+    value[4] = 12
+/*
+    for (v in value){
+        println(v)
+    }
+*/
+    value.forEach {
+        println(it)
+    }
+
+    value.sort()
+    for (index in value.indices){
+        println(value[index])
+    }
+
+}
